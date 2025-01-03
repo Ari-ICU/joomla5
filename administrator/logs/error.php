9a66cea7eacc86a77a176f54c58f0a6af90c6284
Ari-ICU <thoeurn.ratha.kh@gmail.com>
2025-01-03 10:11:50 +0700

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-12-20 10:04:20 UTC
+#Software: Joomla! 5.1.4 Stable [ Kudumisha ] 27-August-2024 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-12-20T10:04:20+00:00	INFO ::1	joomlafailure	Empty password not allowed.
---
+++ administrator/logs/joomla_scheduler.php
@@ -0,0 +1,35 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-12-14 07:00:07 UTC
+#Software: Joomla! 5.1.4 Stable [ Kudumisha ] 27-August-2024 16:00 GMT
+
+#Fields: date	time	priority	message
+2024-12-14	07:00:07	INFO	Running task#02 'Session GC'.
+2024-12-14	07:00:07	INFO	Task> SessionGC end
+2024-12-14	07:00:07	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2024-12-14	07:00:11	INFO	Running task#03 'Update Notification'.
+2024-12-14	07:00:13	INFO	Task> UpdateNotification end
+2024-12-14	07:00:13	INFO	Successfully finished task#03 in 1.34 (net 1.34) seconds.
+2024-12-18	12:50:59	INFO	Running task#02 'Session GC'.
+2024-12-18	12:50:59	INFO	Task> SessionGC end
+2024-12-18	12:50:59	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2024-12-18	12:51:26	INFO	Running task#03 'Update Notification'.
+2024-12-18	12:51:28	INFO	Task> UpdateNotification end
+2024-12-18	12:51:28	INFO	Successfully finished task#03 in 1.93 (net 1.93) seconds.
+2024-12-20	03:58:15	INFO	Running task#02 'Session GC'.
+2024-12-20	03:58:15	INFO	Task> SessionGC end
+2024-12-20	03:58:15	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2024-12-20	04:03:16	INFO	Running task#03 'Update Notification'.
+2024-12-20	04:03:17	INFO	Task> UpdateNotification end
+2024-12-20	04:03:17	INFO	Successfully finished task#03 in 1.55 (net 1.55) seconds.
+2024-12-22	04:51:50	INFO	Running task#02 'Session GC'.
+2024-12-22	04:51:50	INFO	Task> SessionGC end
+2024-12-22	04:51:50	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2024-12-22	04:52:13	INFO	Running task#03 'Update Notification'.
+2024-12-22	04:52:14	INFO	Successfully finished task#03 in 1.30 (net 1.30) seconds.
+2025-01-03	02:49:28	INFO	Running task#02 'Session GC'.
+2025-01-03	02:49:28	INFO	Task> SessionGC end
+2025-01-03	02:49:28	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2025-01-03	02:49:35	INFO	Running task#03 'Update Notification'.
+2025-01-03	02:49:36	INFO	Task> UpdateNotification end
+2025-01-03	02:49:36	INFO	Successfully finished task#03 in 0.87 (net 0.88) seconds.
